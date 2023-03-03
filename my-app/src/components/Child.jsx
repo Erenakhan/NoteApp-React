6e041ecf714611bde903f4b6ca24499e9df18627
Erenakhan <mustafaakhan794@gmail.com>
2023-03-03 16:16:26 +0300

    some css code changed

--- my-app/src/components/Child.jsx
+++ my-app/src/components/Child.jsx
@@ -1,17 +1,19 @@
 import {useState} from 'react'
 import '../App.css'
-import Notes from './Notes'
+
 
 function Child() {
     const [value,setValue]=useState()
     const [notes,setNotes]=useState([])
-    const [background,setBackground]=useState("blue")
-    const colorBlue=()=>{setBackground('blue') }
-    const colorRed=()=>{setBackground('red') }
-    const colorBlack=()=>{setBackground('black') }
+    const [background,setBackground]=useState("rgb(90, 90, 201)")
+    const colorBlue=()=>{setBackground( "rgb(90, 90, 201)") }
+    const colorGreen=()=>{setBackground( "rgb(76, 168, 108)") }
+    const colorYelleow=()=>{setBackground('yellow') }
     const colorPurple=()=>{setBackground('purple') }
+
     const add =()=>{
-        setNotes((notes)=>[
+        setNotes((notes)=>( 
+          [
           ...notes, // prev notes set to notes
            {
             id:1,
@@ -19,23 +21,27 @@ function Child() {
             value:value,
             backgroundColor: background,
                      } //new notes added
-        ])
+        ]))
         setValue("") //set value 
     }
     return (
     <div className='main'> 
+          <h1>Post-it</h1>
           <div className="input">
             <input type='text' className='input-input' value={value} 
           onChange={((e)=>setValue(e.target.value))} 
-          placeholder="enter your notes here"
+          placeholder="Enter your notes here..."
           style={{backgroundColor: background}} />
-          
-          <button onClick={add}>Add</button>
-            <button className='colorBlue' onClick={colorBlue}></button>
-            <button className='colorRed' onClick={colorRed}></button>
-            <button className='colorBlack' onClick={colorBlack}></button>
+        
+          <div>
+          <button className='colorBlue' onClick={colorBlue}></button>
+            <button className='colorGreen' onClick={colorGreen}></button>
+            <button className='colorYellow' onClick={colorYelleow}></button>
             <button className='colorPurple' onClick={colorPurple}></button>
-            
+          </div>
+          <div>
+          <button onClick={add} className="add">Add</button>
+          </div>
           </div>
         <div className='notes'> 
           {
